948afd4f16bcfa58f22fe10a582823a7809bfa39
arsenaldm14 <39810167+arsenaldm14@users.noreply.github.com>
2022-08-02 17:50:46 -0700

    Update and rename Training to Training.py

---
+++ Training.py
@@ -0,0 +1,13 @@
+print(hello world
+
+youtuber = "" # some string variable
+      
+adj = input("adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous person: ")      
+
+madlib = f"computer programming is so {adj}! it makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous person}!"
+      
+print(madlib)      
